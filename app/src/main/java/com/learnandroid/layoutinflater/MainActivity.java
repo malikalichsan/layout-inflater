7c5c391a079793d10b8d37063e6ff68d2b4a9bbf
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-10 11:38:30 +0700

    Layout Inflater Fix

--- app/src/main/java/com/learnandroid/layoutinflater/MainActivity.java
+++ app/src/main/java/com/learnandroid/layoutinflater/MainActivity.java
@@ -2,12 +2,24 @@ package com.learnandroid.layoutinflater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout mainLayout;
+    View view1, view2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = (LinearLayout)findViewById(R.id.main);
+        view1 = getLayoutInflater().inflate(R.layout.sub1, mainLayout, false);
+        view2 = getLayoutInflater().inflate(R.layout.sub2, mainLayout, false);
+
+        mainLayout.addView(view2);
+        mainLayout.addView(view1);
     }
 }
